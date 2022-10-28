18173b3caa72905fa66fce30a40384d82f6506e7
Hairybaldrat <102002527+Hairybaldrat@users.noreply.github.com>
2022-10-28 17:12:11 +1300

    Fixed Bug
    Stopped players ability to shoot in pause screen

--- CoKeRo Project/Assets/Scripts/PlayerController.cs
+++ CoKeRo Project/Assets/Scripts/PlayerController.cs
@@ -49,63 +49,67 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveInput.x = Input.GetAxisRaw("Horizontal");
-        moveInput.y = Input.GetAxisRaw("Vertical");
-
-        moveInput.Normalize();
-
-        //transform.position += new Vector3(moveInput.x * Time.deltaTime * moveSpeed, moveInput.y * Time.deltaTime * moveSpeed);
-        rb.velocity = moveInput * activeMoveSpeed;
-
-        Vector3 mousePos = Input.mousePosition;
-        Vector3 screenPoint = cam.WorldToScreenPoint((transform.localPosition));
-
-        if(mousePos.x < screenPoint.x)
+        if (!isPaused)
         {
-            transform.localScale = new Vector3(-1f,1f,1f);
-            gunArm.localScale = new Vector3(-1f,-1f,1f);
-        }
-        else
-        {
-            transform.localScale = Vector3.one;
-            gunArm.localScale = Vector3.one;
-        }
+            moveInput.x = Input.GetAxisRaw("Horizontal");
+            moveInput.y = Input.GetAxisRaw("Vertical");
 
-        //rotate gun arm
-        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
-        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
-        gunArm.rotation = Quaternion.Euler(0, 0, angle);
+            moveInput.Normalize();
+
+            //transform.position += new Vector3(moveInput.x * Time.deltaTime * moveSpeed, moveInput.y * Time.deltaTime * moveSpeed);
+            rb.velocity = moveInput * activeMoveSpeed;
+
+            Vector3 mousePos = Input.mousePosition;
+            Vector3 screenPoint = cam.WorldToScreenPoint((transform.localPosition));
+
+            if (mousePos.x < screenPoint.x)
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+                gunArm.localScale = new Vector3(-1f, -1f, 1f);
+            }
+            else
+            {
+                transform.localScale = Vector3.one;
+                gunArm.localScale = Vector3.one;
+            }
+
+            //rotate gun arm
+            Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+            float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+            gunArm.rotation = Quaternion.Euler(0, 0, angle);
 
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
-            shotCounter = fireRate;
-        }
-
-        if (Input.GetMouseButton(0))
-        {
-            shotCounter -= Time.deltaTime;
-            if(shotCounter <= 0)
+            if (Input.GetMouseButtonDown(0))
             {
                 Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
                 shotCounter = fireRate;
             }
-        }
 
-        
-        if(Input.GetKeyDown(KeyCode.Space)) 
-        {
-            if(dashCoolCounter <= 0 && dashCounter<= 0)
+            if (Input.GetMouseButton(0))
             {
-                activeMoveSpeed = dashSpeed;
-                dashCounter = dashLength;
-
-                animator.SetTrigger("Dash");
-                PlayerHealthController.instance.MakeInv(dashIframes);
+                shotCounter -= Time.deltaTime;
+                if (shotCounter <= 0)
+                {
+                    Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+                    shotCounter = fireRate;
+                }
             }
-            
+
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (dashCoolCounter <= 0 && dashCounter <= 0)
+                {
+                    activeMoveSpeed = dashSpeed;
+                    dashCounter = dashLength;
+
+                    animator.SetTrigger("Dash");
+                    PlayerHealthController.instance.MakeInv(dashIframes);
+                }
+
+            }
         }
+      
 
         //Switch Mode of pause Menu
         if (Input.GetKeyDown(KeyCode.Escape))
