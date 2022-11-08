b04e3004bcf2e95f6ca95b229b8e5d4156ffb451
Hairybaldrat <102002527+Hairybaldrat@users.noreply.github.com>
2022-11-09 03:33:54 +1300

    Music and menu stuff

---
+++ CoKeRo Project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource levelMusic, gameOverMusic, winMusic;
+    public static AudioManager instance;
+    public AudioSource[] sfx;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayGameOver()
+    {
+        levelMusic.Stop();
+        gameOverMusic.Play();
+    }
+
+    public void PlayLevelWin()
+    {
+        levelMusic.Stop();
+        winMusic.Play();
+    }
+
+    public void PlaySFX(int sfxToPLay)
+    {
+        sfx[sfxToPLay].Stop();
+        sfx[sfxToPLay].Play();
+
+    }
+}
