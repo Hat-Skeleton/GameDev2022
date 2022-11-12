4ef8f47cd4b913fc597668592118759fb93d0bce
Hairybaldrat <102002527+Hairybaldrat@users.noreply.github.com>
2022-11-12 17:50:07 +1300

    Fixed obvious bug
    :+1:

--- CoKeRo Project/Assets/Scripts/DontDestroyMenuStuff.cs
+++ CoKeRo Project/Assets/Scripts/DontDestroyMenuStuff.cs
@@ -1,21 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroyMenuStuff : MonoBehaviour
 {
     static DontDestroyMenuStuff instance;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        if(instance != null)
+            if (instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+    }
+
+    private void Update()
+    {
+        if (!SceneManager.GetActiveScene().name.Equals("MainMenu") && !SceneManager.GetActiveScene().name.Equals("HowToPlay"))
         {
-            Destroy(gameObject);
-        }
-        else
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
+            Destroy(this.gameObject);
         }
     }
 }
