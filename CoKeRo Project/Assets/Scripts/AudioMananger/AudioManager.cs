f6a950b873ba6efd7906d5a0caf642db4a82ccd4
Hairybaldrat <102002527+Hairybaldrat@users.noreply.github.com>
2022-11-13 17:52:11 +1300

    Fixed multiple issues
    Sound, build order

--- CoKeRo Project/Assets/Scripts/AudioMananger/AudioManager.cs
+++ CoKeRo Project/Assets/Scripts/AudioMananger/AudioManager.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
@@ -12,7 +11,14 @@ public class AudioManager : MonoBehaviour
 
     public void Awake()
     {
-           instance = this;
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
     }
     // Start is called before the first frame update
     void Start()
