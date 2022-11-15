78e11d99ef7bc970beb761977e56d2a297301ed7
Hairybaldrat <102002527+Hairybaldrat@users.noreply.github.com>
2022-11-16 01:15:12 +1300

    Added timer to tut
    Sleep time

--- CoKeRo Project/Assets/Scripts/StartScreen.cs
+++ CoKeRo Project/Assets/Scripts/StartScreen.cs
@@ -1,22 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
 public class StartScreen : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject Screen;
+    public TextMeshProUGUI starttext;
+
+    private float counter;
     void Start()
     {
-        
+        counter = 5;
+        PlayerController.instance.enabled = false;
+        starttext.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey)
+        counter -= Time.deltaTime;
+        if (counter <= 0)
         {
-            Destroy(Screen);
+            starttext.enabled = true;
+            PlayerController.instance.enabled = true;
+            if (Input.anyKey)
+            {
+                Destroy(Screen);
+            }
         }
     }
 }
